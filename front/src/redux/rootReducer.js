b125b21707bad084dce3064fa88ddcdb84216bb1
Masashige-T <m.t0719@outlook.jp>
2020-06-11 23:32:47 +0900

    スケジュールとダイアログに関するaction, reducer実装

--- front/src/redux/rootReducer.js
+++ front/src/redux/rootReducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 import calendarReducer from "./calendar/reducer";
+import addaddScheduleReducer from "./addSchedule/reducer";
 
 // 複数のreducerを一つにまとめる
 // combineReducerの引数は{state名: reducer} となる
-const rootReducer = combineReducers({ calendar: calendarReducer });
+const rootReducer = combineReducers({
+  calendar: calendarReducer,
+  addSchedule: addaddScheduleReducer,
+});
 
 export default rootReducer;
